1293fc3a1c674d232d671e5d72a8efc96e5aae04
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-07-17 11:21:46 +0300

    July 17-2024

---
+++ 217.Contains_Duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for i in nums:
+            if i in seen:
+                return True
+            seen.add(i)
+        return False
